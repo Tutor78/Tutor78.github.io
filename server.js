3ea63fd40e7c3f7fd0309dab092f12aaa1802755
Michael Tutor <tutor.michael78@gmail.com>
2021-08-31 16:21:16 -0500

    built index page

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('temp'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './temp/index.html'));
+    res.sendFile(path.join(__dirname, './public/html/index.html'));
 });
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/html/about.html'));
+});
+
+app.get('/portfolio', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/html/portfolio.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/html/contact.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/html/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Server is now running on port ${PORT}`);
